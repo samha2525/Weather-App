c9913dfb90b4f3f673da53d5260db7c3465f9afa
samha <samhamehboob@gmail.com>
2025-10-07 11:56:21 -0700

    full code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+       
+        | Developed by{" "}
+       
+          Samha Mehboob
+         |
+        
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "4420ef9e5478091ac0b306d099a27094",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
